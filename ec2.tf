649508ba2a46839d7c17322986fccbdbece8eee0
ubhatia-coder <ubhatia1988@gmail.com>
2026-06-13 22:26:17 +0530

    added tags

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,11 @@
 resource "aws_instance" "webserver" {
   ami                    = "mi-06c77cb49ac92a541"
   instance_type          = "t3.micro"
+  tags = {
+    Name        = "Primary-Web-Server"
+    Environment = "Production"
+    Owner       = "DevOpsTeam"
+  }
 }
 provider "aws" {
   profile                = "build"
